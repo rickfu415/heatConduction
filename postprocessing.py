7e7b4e44025000544c4fade18f16a8857ea1aa3e
rickfu415 <coolfurui@gmail.com>
2026-03-17 15:19:44 -0400

    Upated a bug for plotting.

--- postprocessing.py
+++ postprocessing.py
@@ -27,7 +27,7 @@ def evolutionField(results):
     X, Y = np.meshgrid(X, Y)
     
     fig = plt.figure(figsize=(8,6))
-    ax = fig.gca(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel('x, m')
     ax.set_ylabel('Time, s')
     ax.set_zlabel('Temperature, K')
