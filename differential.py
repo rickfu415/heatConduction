cd078af082dfe7d2180384a5d30983ca7cca7293
rickfu415 <coolfurui@gmail.com>
2026-03-18 17:25:53 -0400

    Added differential file. working on time reverse loop

--- differential.py
+++ differential.py
@@ -13,17 +13,22 @@ def main(para, cache):
     """ 
     Main function
 
-
     """
     print("Start Adjoint Calculation")
     target_temperature = para['back_wall_temperature_target']
-
-
-
-
-
-
+    log = cache['Log']
+    timeSteps = log.index
+    # reverse time steps for adjoint calculation
+    timeSteps = timeSteps[::-1]
+    for time in timeSteps:
+        T_distribution = cache['TProfile'][:,time]
+        loss = 0.5 * (T_distribution[-1] - target_temperature)**2
+        print('Time: ' + str(time),
+              'T_L temperature: ', 
+              round(T_distribution[-1], 3),
+              'Loss: ', round(loss, 3) )
     return
+
 
 if __name__ == "__main__":
     para = parameter.main()
